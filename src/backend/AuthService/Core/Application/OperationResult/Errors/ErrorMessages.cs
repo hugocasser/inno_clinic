0418a7e6d2007e6060c9531c64cbefc77646a2c0
Артём Герасимюк <hugochacasser@gmail.com>
2024-05-22 15:30:33 +0300

    Add resources for error messages

--- src/backend/AuthService/Core/Application/OperationResult/Errors/ErrorMessages.cs
+++ src/backend/AuthService/Core/Application/OperationResult/Errors/ErrorMessages.cs
@@ -2,12 +2,12 @@ namespace Application.OperationResult.Errors;
 
 public static class ErrorMessages
 {
+    public const string? AccessDenied = "You can't access other user data";
+    public const string? EmailAlreadyConfirmed = "Email already confirmed";
+    public const string? InvalidConfirmToken = "Invalid confirm token";
     public const string? InvalidEmailOrPassword = "Invalid email or password";
+    public const string? InvalidRefreshToken = "Invalid refresh token, please login again";
     public const string? UserAlreadyExists = "User already exists";
     public const string? UserNotHaveSuitableRole = "User not have suitable role";
-    public const string? InvalidRefreshToken = "Invalid refresh token, please login again";
-    public const string? AccessDenied = "You can't access other user data";
     public const string? UserNotFound = "User not found";
-    public const string? InvalidConfirmToken = "Invalid confirm token";
-    public const string? EmailAlreadyConfirmed = "Email already confirmed";
 }
