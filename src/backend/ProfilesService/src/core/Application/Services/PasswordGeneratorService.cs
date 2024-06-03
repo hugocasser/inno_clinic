301073d63a5bdd80621e19455312043a30ebc4a8
Артём Герасимюк <hugochacasser@gmail.com>
2024-06-03 12:39:48 +0300

    Add ValueStringBuilder, refactor password generator service

--- src/backend/ProfilesService/src/core/Application/Services/PasswordGeneratorService.cs
+++ src/backend/ProfilesService/src/core/Application/Services/PasswordGeneratorService.cs
@@ -1,4 +1,4 @@
-using System.Text;
+using ValueStringBuilder = Application.Common.ValueStringBuilder;
 using Application.Abstractions.Services;
 
 namespace Application.Services;
@@ -9,40 +9,40 @@ public class PasswordGeneratorService : IPasswordGeneratorService
     private const string UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private const string Numbers = "0123456789";
     private const string Specials = "!?*.-@$%&()";
-    private readonly Random _randomizer = new();
-    private readonly StringBuilder _passwordBuilder = new();
+    private readonly Random _random = Random.Shared;
 
     public string GenerateRandomPassword()
     {
-        _passwordBuilder.Append(Letters[_randomizer.Next(Letters.Length)]);
-        _passwordBuilder.Append(UppercaseLetters[_randomizer.Next(UppercaseLetters.Length)]);
-        _passwordBuilder.Append(Numbers[_randomizer.Next(Numbers.Length)]);
-        _passwordBuilder.Append(Specials[_randomizer.Next(Specials.Length)]);
+        var passwordBuilder = new ValueStringBuilder(stackalloc char[32]);
         
-        var passwordLength = _randomizer.Next(16, 29);
+        passwordBuilder.Append(Letters[_random.Next(Letters.Length)]);
+        passwordBuilder.Append(UppercaseLetters[_random.Next(UppercaseLetters.Length)]);
+        passwordBuilder.Append(Numbers[_random.Next(Numbers.Length)]);
+        passwordBuilder.Append(Specials[_random.Next(Specials.Length)]);
+        
+        var passwordLength = _random.Next(16, 29);
         
         for (var i = 0; i < passwordLength; i++)
         {
-            switch (_randomizer.Next(4))
+            switch (_random.Next(4))
             {
                 case 0:
-                    _passwordBuilder.Append(Letters[_randomizer.Next(Letters.Length)]);
+                    passwordBuilder.Append(Letters[_random.Next(Letters.Length)]);
                     break;
                 case 1:
-                    _passwordBuilder.Append(UppercaseLetters[_randomizer.Next(UppercaseLetters.Length)]);
+                    passwordBuilder.Append(UppercaseLetters[_random.Next(UppercaseLetters.Length)]);
                     break;
                 case 2:
-                    _passwordBuilder.Append(Numbers[_randomizer.Next(Numbers.Length)]);
+                    passwordBuilder.Append(Numbers[_random.Next(Numbers.Length)]);
                     break;
                 case 3:
-                    _passwordBuilder.Append(Specials[_randomizer.Next(Specials.Length)]);
+                    passwordBuilder.Append(Specials[_random.Next(Specials.Length)]);
                     break;
             }
         }
         
-        var password = _passwordBuilder.ToString();
-        _passwordBuilder.Clear();
-        
+        var password = passwordBuilder.ToString();
+
         return password;
     }
 }
