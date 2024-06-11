e273217a93aea6f8f5568bfe2d79f7a9781751ee
Артём Герасимюк <hugochacasser@gmail.com>
2024-06-11 22:11:10 +0300

    init project

---
+++ src/frontend/AdminDesktopApplication/Presentation/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Presentation;
+
+public partial class App 
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/frontend/AdminDesktopApplication/Presentation/Pages/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation.Pages;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
