0f035ef89f5f5955927f638db699a79f3ad5a7e4
Артём Герасимюк <hugochacasser@gmail.com>
2024-06-25 13:12:40 +0300

    add endpoints

--- src/backend/ServicesApi/Presentation/Extensions/ProgramExtension.cs
+++ src/backend/ServicesApi/Presentation/Extensions/ProgramExtension.cs
@@ -1,6 +1,31 @@
+using BLL;
+using DLL;
+using Presentation.Endpoints;
+
+namespace Presentation.Extensions;
+
+public static class ProgramExtension
+{
+    public static WebApplicationBuilder ConfigureBuilder(this WebApplicationBuilder builder)
+    {
         builder
             .Services
             .AddDataLayer()
             .AddBusinessLayer();
+        
+        return builder;
+    }
+
+    public static WebApplication ConfigureApplication(this WebApplication app)
+    {
         app.MapServicesEndpoints();
         app.MapSpecializationsEndpoints();
+        
+        return app;
+    }
+
+    public static Task RunApplicationAsync(this WebApplication app)
+    {
+        return app.RunAsync();
+    }
+}
