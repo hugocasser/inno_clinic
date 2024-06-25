7eeddda202229370d8c65225e5c13189047ebada
Артём Герасимюк <hugochacasser@gmail.com>
2024-06-26 02:54:42 +0300

    define models

---
+++ src/backend/AppointmentsService/src/Core/Domain/Models/Appointment.cs
@@ -0,0 +1,12 @@
+namespace Domain.Models;
+
+public class Appointment
+{
+    public Guid Id { get; set; }
+    public Guid DoctorId { get; set; }
+    public Guid PatientId { get; set; }
+    public Guid ServiceId { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public bool IsApproved { get; set; }
+}
---
+++ src/backend/AppointmentsService/src/Core/Domain/Models/Result.cs
@@ -0,0 +1,10 @@
+namespace Domain.Models;
+
+public class Result
+{
+    public Guid Id { get; set; }
+    public string Complaints { get; set; } = string.Empty;
+    public string Conclusion { get; set; } = string.Empty;
+    public string Recommendation { get; set; } = string.Empty;
+    public Guid AppointmentId { get; set; }
+}
